a24ab9d7265076363c8c1947006e7273b8fb125f
oshinyil <taneyoroshi@gmail.com>
2018-10-23 11:08:26 +0800

    Created unit test project.

---
+++ OshiSeoAnalyser.Core.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("OshiSeoAnalyser.Core.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("OshiSeoAnalyser.Core.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("55d6ca23-e5ad-4f2a-a3d7-816db2f6e45e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ OshiSeoAnalyser.Core.Test/SeoAnalyserTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace OshiSeoAnalyser.Core.Test
+{
+    [TestClass]
+    public class SeoAnalyserTests
+    {
+        [TestMethod]
+        public void Analyse_GivenText_ReturnsWordOccurences()
+        {
+            const string rawText = "I love you but I hate you. I really love you.";
+
+            var wordOccurences = SeoAnalyser.Analyse(rawText);
+
+            Assert.AreEqual(2, wordOccurences["love"]);
+            Assert.AreEqual(1, wordOccurences["hate"]);
+            Assert.IsFalse(wordOccurences.ContainsKey("you"));
+        }
+
+        [TestMethod]
+        public void Analyse_GivenEmptyString_ReturnsEmptyDictionary()
+        {
+            const string rawText = "";
+
+            var wordOccurences = SeoAnalyser.Analyse(rawText);
+            
+            Assert.AreEqual(0, wordOccurences.Count);
+        }
+    }
+}
